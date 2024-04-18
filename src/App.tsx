e205b249410216b8a8211b382bacb8437ae9f6da
EM-90 <94170154+EM-90@users.noreply.github.com>
2024-04-18 10:11:20 +0200

    Foundation of exam project 2

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'; 
+import './index.css'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h1 className="text-skin-primary text-3xl font-bold">Holidaze</h1>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      textColor: {
+        skin: {
+          primary: 'var(--primary-color)',
+          hoverColour: 'var(--hover-color)',
+          liAnimateColor: 'var(--list-item-animation-color)',
+          editColor: 'var(--edit-color)',
+          tagTextColor: 'var(--tag-text-color)',
+          deleteColor: 'var(--delete-color)',
+
+          mutedText: 'var(--gray-text)',
+        }
+      },
+      backgroundColor: {
+        skin: {
+          deleteBg: 'var(--delete-bg-color)',
+          infoBg: 'var(--info-modal-bg)',
+          editBg: 'var(--edit-bg-color)',
+          createBg: 'var(--background-color)',
+          tagBg: 'var( --tag-bg-color)',
+        }
+      },
+      borderColor: {
+        skin: {
+          innerBorderNewLi: 'var(--inner-color-new-item)',
+          liAnimateColor: 'var(--list-item-animation-color)',
+        }
+      }
+      
+    },
+  },
+  plugins: [],
+}
+
