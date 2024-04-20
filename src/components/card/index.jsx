65e3778842604b2e1d31417baf6dd3cdc71e3c7e
EM-90 <94170154+EM-90@users.noreply.github.com>
2024-04-20 20:21:55 +0200

    Added styling to the card and a responsive container

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,14 +1,22 @@
+import { HiOutlineStar } from "react-icons/hi";
+
 
 function Card({ item }) {
   return (
-    <div className="card">
-      <div className="imageContainer">
-        <img className="object-cover w-full " src={item.media[0].url} alt={item.name} />
+    <div className="card cursor-pointer">
+      <div className="imageContainer h-60">
+        <img className="object-cover w-full h-full rounded-md  " src={item.media[0].url} alt={item.name} />
       </div>
-      <div>
-        <h4>{item.name}</h4>
-        <div>
-          <p>{item.rating}</p>
+      <div className="textContentContainer flex flex-col gap-1 py-2">
+        <div className="titleAndRating flex item-center justify-between">
+          <h4 className=" text-xl font-semibold">{item.name}</h4>
+          <div className="flex items-center">
+            <HiOutlineStar />
+            <p className="pl-1 font-medium">{item.rating}/5</p>
+          </div>
+        </div>
+        <div className="w-full">
+         <p className="price text-xl text-wrap">Price:<span className="itemPrice text-skin-primary text-2xl font-bold pl-2 ">{item.price}kr </span>night</p>
         </div>
       </div>
     </div>
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -5,10 +5,10 @@ import { baseLink, venues } from "../../api/linkConst/Index";
 function Home() {
   return (
     <div className="container mx-auto my-7 px-7">
-      <h1>Venues</h1>
+      <h1 className=" text-5xl font-light">Venues</h1>
       <FetchData url={baseLink + venues}>
         {(data) => (
-          <div className=" container flex-grow mx-auto grid grid-cols-3 w-full my-7 gap-4">
+          <div className="container mx-auto grid xl:grid-cols-3 lg:grid-cols-2 gap-5 py-8">
             {data.map((item) => (
               <Card key={item.id} className="card" item={item} />
             ))}
